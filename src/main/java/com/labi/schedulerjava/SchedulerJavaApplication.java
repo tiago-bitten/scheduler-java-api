cc85114e8658103a707bd84b821ec1e44fbb82db
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-12-28 19:48:31 -0300

    feat: send initial files

---
+++ src/main/java/com/labi/schedulerjava/SchedulerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.labi.schedulerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labi/schedulerjava/SchedulerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labi.schedulerjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
