36172a1e2cfb8290ca351f905d0be579603024e4
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-12-28 20:28:53 -0300

    feat: add CreateMinistryDto dto

---
+++ src/main/java/com/labi/schedulerjava/dtos/CreateMinistryDto.java
@@ -0,0 +1,7 @@
+package com.labi.schedulerjava.dtos;
+
+public record CreateMinistryDto(
+        String name,
+        String description
+) {
+}
--- src/main/java/com/labi/schedulerjava/service/MinistryService.java
+++ src/main/java/com/labi/schedulerjava/service/MinistryService.java
@@ -1,6 +1,7 @@
 package com.labi.schedulerjava.service;
 
 import com.labi.schedulerjava.domain.Ministry;
+import com.labi.schedulerjava.dtos.CreateMinistryDto;
 import com.labi.schedulerjava.repository.MinistryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
