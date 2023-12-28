22aa6d5fc71d33a2a40302f78a69fa33cc7d4763
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-12-28 20:28:12 -0300

    feat: add MinistryService service

---
+++ src/main/java/com/labi/schedulerjava/service/MinistryService.java
@@ -0,0 +1,18 @@
+package com.labi.schedulerjava.service;
+
+import com.labi.schedulerjava.domain.Ministry;
+import com.labi.schedulerjava.repository.MinistryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MinistryService {
+
+    @Autowired
+    private MinistryRepository ministryRepository;
+
+    public void create(CreateMinistryDto dto) {
+        Ministry ministry = new Ministry(dto.name(), dto.description());
+        ministryRepository.save(ministry);
+    }
+}
