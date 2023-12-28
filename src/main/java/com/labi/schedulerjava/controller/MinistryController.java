c33208d33d37b8ce50177065e6a15763665ce278
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-12-28 20:30:18 -0300

    feat: add MinistryController controller

---
+++ src/main/java/com/labi/schedulerjava/controller/MinistryController.java
@@ -0,0 +1,25 @@
+package com.labi.schedulerjava.controller;
+
+import com.labi.schedulerjava.dtos.CreateMinistryDto;
+import com.labi.schedulerjava.service.MinistryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/ministries")
+public class MinistryController {
+
+    @Autowired
+    private MinistryService ministryService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> create(@RequestBody CreateMinistryDto dto) {
+        ministryService.create(dto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
