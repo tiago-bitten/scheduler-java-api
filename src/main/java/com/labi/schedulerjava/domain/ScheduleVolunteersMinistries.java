d1fe9b07e739dfb959590d832f0dc94ef1c155dc
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-12-28 23:17:06 -0300

    feat: add ScheduleVolunteersMinistries

---
+++ src/main/java/com/labi/schedulerjava/domain/ScheduleVolunteersMinistries.java
@@ -0,0 +1,22 @@
+package com.labi.schedulerjava.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "schedule_volunteers_ministries")
+public class ScheduleVolunteersMinistries extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "volunteer_ministry_id")
+    private Schedule schedule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private VolunteerMinistry volunteerMinistry;
+}
