d7bdf8429a514338b925ea1a29348e7bb89bd2cd
BuildTools <tiago.sbittencourt@icloud.com>
2024-01-14 19:53:23 -0300

    refactor: improve CreateMinistryUseCase

--- src/main/java/com/labi/schedulerjava/adapters/web/MinistryController.java
+++ src/main/java/com/labi/schedulerjava/adapters/web/MinistryController.java
@@ -1,11 +1,16 @@
 package com.labi.schedulerjava.adapters.web;
 
+import com.labi.schedulerjava.core.usecases.ministry.CreateMinistryUseCase;
 import com.labi.schedulerjava.dtos.CreateMinistryDto;
+import com.labi.schedulerjava.dtos.ReadMinistryDto;
 import com.labi.schedulerjava.service._MinistryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/ministries")
@@ -14,9 +19,12 @@ public class MinistryController {
     @Autowired
     private _MinistryService ministryService;
 
+    @Autowired
+    private CreateMinistryUseCase createMinistryUseCase;
+
     @PostMapping("/create")
-    public ResponseEntity<Void> create(@RequestBody CreateMinistryDto dto) {
-        ministryService.create(dto);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    public ResponseEntity<CreateMinistryUseCase.OutputValues> create(@RequestBody CreateMinistryDto dto) {
+        createMinistryUseCase.execute(new CreateMinistryUseCase.InputValues(dto));
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/labi/schedulerjava/core/usecases/ministry/CreateMinistryUseCase.java
+++ src/main/java/com/labi/schedulerjava/core/usecases/ministry/CreateMinistryUseCase.java
@@ -2,22 +2,37 @@ package com.labi.schedulerjava.core.usecases.ministry;
 
 import com.labi.schedulerjava.adapters.persistence.MinistryRepository;
 import com.labi.schedulerjava.core.domain.model.Ministry;
+import com.labi.schedulerjava.core.usecases.UseCase;
 import com.labi.schedulerjava.dtos.CreateMinistryDto;
 import com.labi.schedulerjava.core.domain.exception.BusinessRuleException;
+import com.labi.schedulerjava.dtos.ReadMinistryDto;
+import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CreateMinistryUseCase {
+public class CreateMinistryUseCase extends UseCase<CreateMinistryUseCase.InputValues, CreateMinistryUseCase.OutputValues> {
 
     @Autowired
     private MinistryRepository ministryRepository;
 
-    public void create(CreateMinistryDto dto) {
-        if (ministryRepository.findByName(dto.name()).isPresent())
+    @Override
+    public OutputValues execute(InputValues input) {
+        if (ministryRepository.findByName(input.dto.name()).isPresent()) {
             throw new BusinessRuleException("Este ministério já está cadastrado");
+        }
 
-        Ministry ministry = new Ministry(dto.name(), dto.description());
+        Ministry ministry = new Ministry(input.dto.name(), input.dto.description());
         ministryRepository.save(ministry);
+        return new OutputValues();
+    }
+
+    @Value
+    public static class InputValues implements UseCase.InputValues {
+        private CreateMinistryDto dto;
+    }
+
+    @Value
+    public static class OutputValues implements UseCase.OutputValues {
     }
 }
