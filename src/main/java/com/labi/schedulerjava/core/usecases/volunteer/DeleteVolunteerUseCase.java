115ead8b5d77f174ab6a9f4e5312c4ad9ba25faa
BuildTools <tiago.sbittencourt@icloud.com>
2024-02-11 13:10:32 -0300

    impr: normalize totalVolunteers

--- src/main/java/com/labi/schedulerjava/core/usecases/volunteer/DeleteVolunteerUseCase.java
+++ src/main/java/com/labi/schedulerjava/core/usecases/volunteer/DeleteVolunteerUseCase.java
@@ -2,11 +2,14 @@ package com.labi.schedulerjava.core.usecases.volunteer;
 
 import com.labi.schedulerjava.adapters.persistence.VolunteerRepository;
 import com.labi.schedulerjava.core.domain.exception.BusinessRuleException;
+import com.labi.schedulerjava.core.domain.model.Ministry;
 import com.labi.schedulerjava.core.domain.model.Volunteer;
 import com.labi.schedulerjava.core.usecases.UseCase;
 import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class DeleteVolunteerUseCase extends UseCase<DeleteVolunteerUseCase.InputValues, DeleteVolunteerUseCase.OutputValues> {
@@ -18,6 +21,8 @@ public class DeleteVolunteerUseCase extends UseCase<DeleteVolunteerUseCase.Input
     public OutputValues execute(InputValues input) {
         Volunteer volunteer = volunteerRepository.findById(input.id)
                 .orElseThrow(() -> new BusinessRuleException("O ID " + input.id + " não corresponde a um voluntário cadastrado"));
+
+        volunteer.getVolunteerMinistries().forEach(vm -> vm.getMinistry().setTotalVolunteers(vm.getMinistry().getTotalVolunteers() - 1));
 
         volunteerRepository.delete(volunteer);
 
