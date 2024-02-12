858bbfd5d3fda3b1b5fae6c9dbd4fce38c01bedd
BuildTools <tiago.sbittencourt@icloud.com>
2024-02-12 13:16:32 -0300

    feat: add Mocks

--- src/main/java/com/labi/schedulerjava/SchedulerJavaApplication.java
+++ src/main/java/com/labi/schedulerjava/SchedulerJavaApplication.java
@@ -2,16 +2,24 @@ package com.labi.schedulerjava;
 
 import com.labi.schedulerjava.adapters.persistence.MinistryRepository;
 import com.labi.schedulerjava.adapters.persistence.UserRepository;
+import com.labi.schedulerjava.adapters.persistence.VolunteerMinistryRepository;
+import com.labi.schedulerjava.adapters.persistence.VolunteerRepository;
 import com.labi.schedulerjava.core.domain.model.Ministry;
 import com.labi.schedulerjava.core.domain.model.User;
+import com.labi.schedulerjava.core.domain.model.Volunteer;
+import com.labi.schedulerjava.core.domain.model.VolunteerMinistry;
 import com.labi.schedulerjava.core.domain.service.UserMinistryService;
+import com.labi.schedulerjava.enums.VolunteerOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
 public class SchedulerJavaApplication {
@@ -32,10 +40,60 @@ public class SchedulerJavaApplication {
 	@Autowired
 	private UserMinistryService userMinistryService;
 
+	@Autowired
+	private VolunteerRepository volunteerRepository;
+
+	@Autowired
+	private VolunteerMinistryRepository volunteerMinistryRepository;
+
 	@Bean
 	public Object createMinistry() {
 		Ministry ministry = new Ministry("CEIA", "Ceia principal", "#494949");
-		ministryRepository.save(ministry);
+		Ministry ministry2 = new Ministry("LOUVOR", "Louvor principal", "#AC2020");
+		Ministry ministry3 = new Ministry("DANÇA", "Dança principal", "#6F43C5");
+		Ministry ministry4 = new Ministry("TEATRO", "Teatro principal", "#2FAC95");
+		Ministry ministry5 = new Ministry("CRIANÇAS", "Crianças principal", "#BB1386");
+		Ministry ministry6 = new Ministry("ADOLESCENTES", "Adolescentes principal", "#5C8CDE");
+		ministryRepository.saveAll(List.of(ministry, ministry2, ministry3, ministry4, ministry5, ministry6));
+
+		Volunteer volunteer1 = new Volunteer("Tiago", "Silveira", "11642426954", "48998533335", LocalDate.parse("2003-02-26"), VolunteerOrigin.USER_REGISTERED);
+		Volunteer volunteer2 = new Volunteer("Mariana", "Costa", "12345678901", "47999223344", LocalDate.parse("1995-07-19"), VolunteerOrigin.USER_REGISTERED);
+		Volunteer volunteer3 = new Volunteer("Carlos", "Mendes", "23456789012", "47998877665", LocalDate.parse("1988-11-30"), VolunteerOrigin.USER_REGISTERED);
+		Volunteer volunteer4 = new Volunteer("Julia", "Barbosa", "34567890123", "47997766554", LocalDate.parse("1992-05-15"), VolunteerOrigin.USER_REGISTERED);
+		Volunteer volunteer5 = new Volunteer("Lucas", "Silva", "45678901234", "47996655443", LocalDate.parse("1998-09-10"), VolunteerOrigin.USER_REGISTERED);
+		Volunteer volunteer6 = new Volunteer("Maria", "Santos", "56789012345", "47995544332", LocalDate.parse("2000-12-25"), VolunteerOrigin.USER_REGISTERED);
+		Volunteer volunteer7 = new Volunteer("João", "Oliveira", "67890123456", "47994433221", LocalDate.parse("1990-03-05"), VolunteerOrigin.USER_REGISTERED);
+		Volunteer volunteer8 = new Volunteer("Ana", "Pereira", "78901234567", "47993322110", LocalDate.parse("1996-06-20"), VolunteerOrigin.USER_REGISTERED);
+		Volunteer volunteer9 = new Volunteer("Pedro", "Ribeiro", "89012345678", "47992211009", LocalDate.parse("1994-01-15"), VolunteerOrigin.USER_REGISTERED);
+		Volunteer volunteer10 = new Volunteer("Paula", "Ferreira", "90123456789", "47991100998", LocalDate.parse("1999-04-30"), VolunteerOrigin.USER_REGISTERED);
+
+		volunteerRepository.saveAll(List.of(volunteer1, volunteer2, volunteer3, volunteer4, volunteer5, volunteer6, volunteer7, volunteer8, volunteer9, volunteer10));
+
+		List<Ministry> ministries = List.of(ministry, ministry2, ministry3, ministry4, ministry5, ministry6);
+		List<Volunteer> volunteers = List.of(volunteer1, volunteer2, volunteer3, volunteer4, volunteer5, volunteer6, volunteer7, volunteer8, volunteer9, volunteer10);
+
+		List<VolunteerMinistry> volunteerMinistries = new ArrayList<>();
+		Random random = new Random();
+
+		for (Volunteer volunteer : volunteers) {
+			int numMinistriesForVolunteer = random.nextInt(5);
+
+			List<Ministry> assignedMinistries = new ArrayList<>();
+			for (int i = 0; i < numMinistriesForVolunteer; i++) {
+				Ministry randomMinistry = ministries.get(random.nextInt(ministries.size()));
+
+				if (!assignedMinistries.contains(randomMinistry)) {
+					assignedMinistries.add(randomMinistry);
+					volunteerMinistries.add(new VolunteerMinistry(volunteer, randomMinistry));
+					randomMinistry.setTotalVolunteers(randomMinistry.getTotalVolunteers() + 1);
+				} else {
+					i--;
+				}
+			}
+		}
+
+		volunteerMinistryRepository.saveAll(volunteerMinistries);
+
 		return null;
 	}
 
@@ -47,7 +105,7 @@ public class SchedulerJavaApplication {
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		userRepository.save(user);
 
-		userMinistryService.associate(user, List.of(1L));
+		userMinistryService.associate(user, List.of(1L, 2L, 3L, 4L, 5L, 6L));
 		return null;
 	}
 }
