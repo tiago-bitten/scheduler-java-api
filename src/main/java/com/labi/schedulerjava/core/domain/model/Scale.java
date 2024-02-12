3cd51aa961a14833aa5942fa7c6a67d35171bb40
BuildTools <tiago.sbittencourt@icloud.com>
2024-02-12 13:41:05 -0300

    feat: add Scale model

---
+++ src/main/java/com/labi/schedulerjava/core/domain/model/Scale.java
@@ -0,0 +1,37 @@
+package com.labi.schedulerjava.core.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "scales")
+public class Scale extends BaseEntity {
+
+    @Column(name = "name", nullable = false)
+    private Long maxVolunteers;
+
+    @Column(name = "generated_at", nullable = false, updatable = false)
+    private Instant generatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id", nullable = false)
+    private Schedule schedule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "generated_by_id", nullable = false)
+    private User generatedBy;
+
+    public Scale(Long maxVolunteers, Schedule schedule, User generatedBy) {
+        this.maxVolunteers = maxVolunteers;
+        this.generatedAt = Instant.now();
+        this.schedule = schedule;
+        this.generatedBy = generatedBy;
+    }
+}
