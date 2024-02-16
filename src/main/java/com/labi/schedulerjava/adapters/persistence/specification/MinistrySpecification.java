ec1bac9497675c322ca7f3235bb2bccf4f03d215
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-02-15 23:44:28 -0300

    feat: add hasVolunteer ministry spec

--- src/main/java/com/labi/schedulerjava/adapters/persistence/specification/MinistrySpecification.java
+++ src/main/java/com/labi/schedulerjava/adapters/persistence/specification/MinistrySpecification.java
@@ -1,6 +1,10 @@
 package com.labi.schedulerjava.adapters.persistence.specification;
 
 import com.labi.schedulerjava.core.domain.model.Ministry;
+import com.labi.schedulerjava.core.domain.model.Volunteer;
+import com.labi.schedulerjava.core.domain.model.VolunteerMinistry;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
 
 public class MinistrySpecification {
@@ -9,6 +13,15 @@ public class MinistrySpecification {
         return (root, query, criteriaBuilder) -> {
             if (ministryName == null || ministryName.isEmpty()) return criteriaBuilder.conjunction();
             return criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + ministryName.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<Ministry> hasVolunteer(String volunteerName) {
+        return (root, query, CriteriaBuilder) -> {
+            if (volunteerName == null || volunteerName.isEmpty()) return CriteriaBuilder.conjunction();
+            Join<Ministry, VolunteerMinistry> volunteerMinistryJoin = root.join("volunteerMinistries", JoinType.INNER);
+            Join<VolunteerMinistry, Volunteer> volunteerJoin = volunteerMinistryJoin.join("volunteer", JoinType.INNER);
+            return CriteriaBuilder.like(CriteriaBuilder.lower(volunteerJoin.get("name")), "%" + volunteerName.toLowerCase() + "%");
         };
     }
 }
