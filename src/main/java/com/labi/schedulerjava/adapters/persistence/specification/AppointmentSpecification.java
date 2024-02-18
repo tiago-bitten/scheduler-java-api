2f1a38ae1543dd0bd660d5a8b5ab472d2423f9e9
BuildTools <tiago.sbittencourt@icloud.com>
2024-02-18 12:59:18 -0300

    impr: AppointmentSpecificiation

--- src/main/java/com/labi/schedulerjava/adapters/persistence/specification/AppointmentSpecification.java
+++ src/main/java/com/labi/schedulerjava/adapters/persistence/specification/AppointmentSpecification.java
@@ -1,6 +1,10 @@
 package com.labi.schedulerjava.adapters.persistence.specification;
 
 import com.labi.schedulerjava.core.domain.model.Appointment;
+import com.labi.schedulerjava.core.domain.model.Volunteer;
+import com.labi.schedulerjava.core.domain.model.VolunteerMinistry;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
 
 public class AppointmentSpecification {
@@ -8,7 +12,9 @@ public class AppointmentSpecification {
     public static Specification<Appointment> hasVolunteer(String volunteerName) {
         return (root, query, criteriaBuilder) -> {
             if (volunteerName == null || volunteerName.isEmpty()) return criteriaBuilder.conjunction();
-            return criteriaBuilder.like(root.get("volunteerMinistry").get("volunteer").get("name"), "%" + volunteerName + "%");
+            Join<Appointment, VolunteerMinistry> volunteerMinistryJoin = root.join("volunteerMinistry", JoinType.INNER);
+            Join<VolunteerMinistry, Volunteer> volunteerJoin = volunteerMinistryJoin.join("volunteer", JoinType.INNER);
+            return criteriaBuilder.like(criteriaBuilder.lower(volunteerJoin.get("name")), "%" + volunteerName.toLowerCase() + "%");
         };
     }
 
