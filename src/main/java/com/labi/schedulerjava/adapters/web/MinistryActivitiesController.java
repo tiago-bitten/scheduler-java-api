d9e99f3075b3dc68bf422674c2963dc5092c80d0
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-02-20 22:52:24 -0300

    impr: ActivitiesController

--- src/main/java/com/labi/schedulerjava/adapters/web/MinistryActivitiesController.java
+++ src/main/java/com/labi/schedulerjava/adapters/web/MinistryActivitiesController.java
@@ -1,18 +1,26 @@
 package com.labi.schedulerjava.adapters.web;
 
 import com.labi.schedulerjava.core.usecases.UseCase;
+import com.labi.schedulerjava.core.usecases.ministryactivities.CreateActivityUseCase;
+import com.labi.schedulerjava.dtos.ActivityRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/ministry-activities")
 public class MinistryActivitiesController {
 
+    @Autowired
+    private CreateActivityUseCase createActivityUseCase;
+
     @PostMapping("/create")
-    public ResponseEntity<UseCase.OutputValues> create() {
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    public ResponseEntity<UseCase.OutputValues> create(@RequestBody @Valid ActivityRequest request,
+                                                       @RequestParam Long ministryId) {
+        UseCase.OutputValues outputValues =
+                createActivityUseCase.execute(new CreateActivityUseCase.InputValues(ministryId, request));
+        return new ResponseEntity<>(outputValues, HttpStatus.CREATED);
     }
 }
