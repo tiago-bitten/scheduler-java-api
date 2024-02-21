007cc8a06ec9bb335ac62b630f8a0a45c06913fc
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-02-20 22:41:36 -0300

    feat: add MinistryActivitiesController

---
+++ src/main/java/com/labi/schedulerjava/adapters/web/MinistryActivitiesController.java
@@ -0,0 +1,19 @@
+package com.labi.schedulerjava.adapters.web;
+
+import com.labi.schedulerjava.core.usecases.UseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/ministry-activities")
+public class MinistryActivitiesController {
+
+    @PostMapping("/create")
+    public ResponseEntity<UseCase.OutputValues> create() {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
