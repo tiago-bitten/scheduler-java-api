661365339843cbf03bc2f796392d964e89dfd8b6
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-02-20 22:47:48 -0300

    impr: CreateActivityUseCase

--- src/main/java/com/labi/schedulerjava/core/usecases/ministryactivities/CreateActivityUseCase.java
+++ src/main/java/com/labi/schedulerjava/core/usecases/ministryactivities/CreateActivityUseCase.java
@@ -1,7 +1,12 @@
 package com.labi.schedulerjava.core.usecases.ministryactivities;
 
 import com.labi.schedulerjava.adapters.persistence.MinistryActivitiesRepository;
+import com.labi.schedulerjava.core.domain.exception.BusinessRuleException;
+import com.labi.schedulerjava.core.domain.model.Ministry;
+import com.labi.schedulerjava.core.domain.model.MinistryActivities;
+import com.labi.schedulerjava.core.domain.service.MinistryService;
 import com.labi.schedulerjava.core.usecases.UseCase;
+import com.labi.schedulerjava.dtos.ActivityRequest;
 import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,14 +17,28 @@ public class CreateActivityUseCase extends UseCase<CreateActivityUseCase.InputVa
     @Autowired
     private MinistryActivitiesRepository ministryActivitiesRepository;
 
+    @Autowired
+    private MinistryService ministryService;
+
     @Override
     public OutputValues execute(InputValues input) {
-        return null;
+        Ministry ministry = ministryService.findById(input.ministryId)
+                .orElseThrow(() -> new BusinessRuleException("O ID" + input.ministryId + "não corresponde a um ministério cadastrado"));
+
+        if (input.request.totalVolunteers() < 1)
+            throw new BusinessRuleException("O total de voluntários deve ser maior que 0");
+
+        MinistryActivities ministryActivities = new MinistryActivities(input.request.name(), input.request.totalVolunteers(), ministry);
+
+        ministryActivitiesRepository.save(ministryActivities);
+
+        return new OutputValues();
     }
 
     @Value
     public static class InputValues implements UseCase.InputValues {
         Long ministryId;
+        ActivityRequest request;
     }
 
     @Value
