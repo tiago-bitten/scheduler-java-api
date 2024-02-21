4a154048efdc5d05d23589aa51526e9fa5dbee04
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-02-20 23:00:22 -0300

    feat: add VolunteerActivity

---
+++ src/main/java/com/labi/schedulerjava/core/domain/model/VolunteerActivity.java
@@ -0,0 +1,23 @@
+package com.labi.schedulerjava.core.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "volunteer_activities")
+public class VolunteerActivity extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "volunteer_id", nullable = false)
+    private Volunteer volunteer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ministry_activities_id", nullable = false)
+    private MinistryActivities ministryActivities;
+}
+
