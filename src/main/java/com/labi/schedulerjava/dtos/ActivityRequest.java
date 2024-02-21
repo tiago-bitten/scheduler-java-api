fef944ce77fe9177d19fe807e743b2753185f48a
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-02-20 22:56:35 -0300

    fix: ActivityRequest

--- src/main/java/com/labi/schedulerjava/dtos/ActivityRequest.java
+++ src/main/java/com/labi/schedulerjava/dtos/ActivityRequest.java
@@ -1,12 +1,13 @@
 package com.labi.schedulerjava.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ActivityRequest(
         @NotBlank(message = "O campo nome é obrigatório")
         String name,
 
-        @NotBlank(message = "O campo do número total de voluntários é obrigatório")
+        @NotNull(message = "O campo do número total de voluntários é obrigatório")
         Long totalVolunteers
 ) {
 }
