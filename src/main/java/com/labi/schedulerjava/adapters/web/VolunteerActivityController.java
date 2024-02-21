e95101b564211667cffd87f75d3fd9d8c918ee59
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-02-20 23:08:52 -0300

    impr: VolunteerActivityController

--- src/main/java/com/labi/schedulerjava/adapters/web/VolunteerActivityController.java
+++ src/main/java/com/labi/schedulerjava/adapters/web/VolunteerActivityController.java
@@ -1,5 +1,8 @@
 package com.labi.schedulerjava.adapters.web;
 
+import com.labi.schedulerjava.core.usecases.volunteeractivity.AssignVolunteerActivityUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/volunteer-activities")
 public class VolunteerActivityController {
 
+    @Autowired
+    private AssignVolunteerActivityUseCase assignVolunteerActivityUseCase;
+
     @PostMapping("/assign")
     public ResponseEntity<Void> assign(@RequestParam Long volunteerId, @RequestParam Long activityId) {
-        return ResponseEntity.ok().build();
+        assignVolunteerActivityUseCase.execute(new AssignVolunteerActivityUseCase.InputValues(volunteerId, activityId));
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
