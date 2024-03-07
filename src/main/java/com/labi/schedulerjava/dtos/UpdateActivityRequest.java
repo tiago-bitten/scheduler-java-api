2c137169c3d350ee567c941954c90306fae10a9b
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-03-07 00:14:09 -0300

    impr: updateactivity

--- src/main/java/com/labi/schedulerjava/dtos/UpdateActivityRequest.java
+++ src/main/java/com/labi/schedulerjava/dtos/UpdateActivityRequest.java
@@ -1,7 +1,14 @@
 package com.labi.schedulerjava.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record UpdateActivityRequest(
+
+        @NotBlank(message = "Nome não pode ser em branco")
         String name,
+
+        @NotNull(message = "Total de voluntários não pode ser nulo")
         Long defaultTotalVolunteers
 ) {
 }
